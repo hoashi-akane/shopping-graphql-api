dd410c05cf04daef65cdd3e63ef34dd8f3fd570b
hoashi akane <akane.hoashi@outlook.jp>
2020-05-31 19:04:09 +0900

    first commit

---
+++ graph/resolver.go
@@ -0,0 +1,10 @@
+package graph
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/hoashi-akane/shopping-graphql/graph/model"
+
+type Resolver struct{
+	todos []*model.Todo
+}
