fa9b4198a96c57e9fa039fd13284aedf1c47bbd7
hoashi <akane.hoashi@outlook.jp>
2020-06-11 17:06:45 +0900

    idを取得できるように設定

--- graph/model/goods.go
+++ graph/model/goods.go
@@ -1,6 +1,7 @@
 package model
 
 type Goods struct{
+	Id int `json:"id"`
 	GoodsName string `json:"name"`
 	Price int `json:"price"`
 	Stock int `json:"stock"`
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -14,10 +14,6 @@ import (
 	"github.com/hoashi-akane/shopping-graphql/graph/model"
 )
 
-func (r *goodsResolver) ID(ctx context.Context, obj *model.Goods) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
 	todo := &model.Todo{
 		Text:   input.Text,
@@ -47,7 +43,7 @@ func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
 }
 
 func (r *queryResolver) Goodes(ctx context.Context) ([]*model.Goods, error) {
-	rows, err := r.DB.Query("SELECT goods_name, price, stock FROM goods;")
+	rows, err := r.DB.Query("SELECT id, goods_name, price, stock FROM goods;")
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +53,7 @@ func (r *queryResolver) Goodes(ctx context.Context) ([]*model.Goods, error) {
 		// 参照渡しをすることでコピー処理を発生させない。
 		// 構造体の配列に入れるための一時領域のため書き換えても問題ない。
 		goods = &model.Goods{}
-		err := rows.Scan(&goods.GoodsName, &goods.Price, &goods.Stock)
+		err := rows.Scan(&goods.Id, &goods.GoodsName, &goods.Price, &goods.Stock)
 		if err != nil {
 			panic(fmt.Errorf("DBエラー"))
 		}
@@ -70,9 +66,6 @@ func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, 
 	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
 }
 
-// Goods returns generated.GoodsResolver implementation.
-func (r *Resolver) Goods() generated.GoodsResolver { return &goodsResolver{r} }
-
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
@@ -82,23 +75,6 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 // Todo returns generated.TodoResolver implementation.
 func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
 
-type goodsResolver struct{ *Resolver }
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type todoResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *goodsResolver) Price(ctx context.Context, obj *model.Goods) (int, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *goodsResolver) GoodsName(ctx context.Context, obj *model.Goods) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-func (r *goodsResolver) Stock(ctx context.Context, obj *model.Goods) (int, error) {
-	panic(fmt.Errorf("not implemented"))
-}
