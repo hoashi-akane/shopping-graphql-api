a316a6b1913f33ebab9026677fc5f132764dc1f2
hoashi <akane.hoashi@outlook.jp>
2020-06-14 20:56:35 +0900

    変更：goodsモデルにブランドIdをnull許容で持たせた

--- graph/model/goods.go
+++ graph/model/goods.go
@@ -1,8 +1,16 @@
 package model
 
-type Goods struct{
-	Id int `json:"id"`
-	GoodsName string `json:"name"`
-	Price int `json:"price"`
-	Stock int `json:"stock"`
+type Goods struct {
+	ID        int     `json:"id"`
+	GoodsName string  `json:"goodsName"`
+	Price     int     `json:"price"`
+	Stock     int     `json:"stock"`
+	BrandId   *int `json:"brandId"`
+}
+
+type NewGoods struct {
+	GoodsName string `json:"goodsName"`
+	Price     int    `json:"price"`
+	Stock     int    `json:"stock"`
+	BrandId	  *int	`json:"brandId"`
 }
